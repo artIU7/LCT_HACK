c1fbb7b5202f831b9a15f4ec1f184661bc73415a
brazilec22 <stratienko.artem@gmail.com>
2020-11-01 04:28:02 +0300

    auto commit rep - metro

---
+++ client/metro_maps/metro_maps/Application/MapsViewClass/ViewController/MapSceneController.swift
@@ -0,0 +1,31 @@
+//
+//  MapSceneController.swift
+//  metro_maps
+//
+//  Created by Artem Stratienko on 01.11.2020.
+//
+
+import UIKit
+import NMAKit
+
+
+class MapSceneController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- client/metro_maps/metro_maps/Application/Model/AppDelegate.swift
+++ client/metro_maps/metro_maps/Application/Model/AppDelegate.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import NMAKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    let appID = "7T8UgnMEkQDNCeJH182A"
+    let appCODE = "wfyqs2ZxyVaiTgdkqDnIcQ"
+    let licenseKEY = "kDvRN5xPz055M13RA0wgvRIfffbsbwtmMgO0KKbdTntw8+o4J0FphfHQ+eWlsdTDv0hrpouNhVIwJ/YMGMviTvKxK3sOPNAuDK5R8uWhio3iZoORiVPWhyqfSt+4NmhEYBWTdJVE3F6gD/HKuiDoKmGupllA6cE3m8OMQzjyDskJQXor91ipNwmE3oGBy/A+A+zR4CO6SYydzlFPh+yF2TVF42DyKPF7km+Dk7m02IjsSBxiIb0UKZx5ldqnG5R+5MAhn5x6vcfbk1YynNVkd8qCk0LXuyBbXA87gk0HUxypFnaWUMqgoBylVJWAliF3p2X7yVX+s8wIBNULI9/c/uSLWPTLb7zqqHIn1lyMeR/MoLBNLzY+h4LBeMWX+0tT/jWYSjubQ2/q14j++CSw9LvHcYaADHv0wJYf0jzJaTyCK0EOb2cBgaqg4QRjSDIIKyrFfbVn2SEmAutZYnhl0Geb9mEVAFq6ljX3THJkrslvXCt2vrFLzjmCmSDRDh0mJ2Nwugj+D6rXS2QDCYE/yBSdekfYe3z/BiJwD5Bfj2YOQaygKNFXy1CCaGZOzFp0kbXdQFWfIeLyDoM0M3cL+luVM/0gLQu5f3xwyUSIQXikJedr141Cf9xRjSWpgfe6Ri/6G/VPAI+LFOHu7jHHfQPfr2dhKVTj+KLA2/xbbCs="
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        NMAApplicationContext.setAppId(appID,
+                                       appCode: appCODE,
+                                       licenseKey: licenseKEY)
         return true
     }
 
